6ea73c538149bde08dad6b6201237749da261ad6
= <=>
2022-08-03 16:44:36 -0500

    Lecture 36

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,15 @@
   <h1>Ninja Reaction Timer</h1>
   <button @click="start" :disabled="isPlaying">play</button>
   <Block v-if="isPlaying" :delay="delay" @end="endGame" />
-  <p v-if="showResults">Reaction Time: {{ score }} ms </p>
+  <Results v-if="showResults" :score="score"/>
 </template>
 
 <script>
 import Block from './components/Block.vue'
+import Results from './components/Results.vue'
 export default {
   name: 'App',
-  components: { Block },
+  components: { Block, Results },
   data() {
     return {
       isPlaying: false,
